32b34f2f097019929c6dc5c0a9698f990e33df87
Esseh12 <ese.gbenmo@salario.africa>
2025-12-03 11:09:11 +0100

    updated cron job logic

--- app/api/sendStats/route.js
+++ app/api/sendStats/route.js
@@ -3,20 +3,12 @@ import Reservation from '@/models/Reservation';
 import nodemailer from 'nodemailer';
 
 export async function GET(req) {
-	// Check CRON_SECRET
-	const authHeader = req.headers.get('Authorization');
-	if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-		return new Response('Unauthorized', { status: 401 });
-	}
-
 	try {
-		await connectDB(); // connect to MongoDB
+		await connectDB();
 		console.log('DB connected for stats');
 
-		// Get total reservations
 		const totalReservations = await Reservation.countDocuments();
 
-		// Send email
 		const transporter = nodemailer.createTransport({
 			service: 'gmail',
 			auth: {
@@ -27,7 +19,7 @@ export async function GET(req) {
 
 		await transporter.sendMail({
 			from: process.env.GMAIL_USERNAME,
-			to: 'primedevng@gmail.com',
+			to: 'primedevng@gmail.com', // your target email
 			subject: 'Reservation Stats Update',
 			html: `
         <h2>Quickteller Campaign Stats</h2>
