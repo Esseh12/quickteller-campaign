364e087b2d43a4b087dd85767cc319bc0208559c
Esseh12 <ese.gbenmo@salario.africa>
2025-12-05 15:21:50 +0100

    removed phone number check for mai validation: user can create with same number but not same mail

--- app/api/reservations/route.js
+++ app/api/reservations/route.js
@@ -16,7 +16,7 @@ export async function POST(req) {
 
 		// Check duplicates
 		const existing = await Reservation.findOne({
-			$or: [{ email }, { phone }],
+			$or: [{ email }],
 		});
 
 		if (existing) {
